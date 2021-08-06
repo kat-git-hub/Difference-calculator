9b3a26e7827c0684cb95dacc404486b3e68e743b
kat-git-hub <humanpizzza@gmail.com>
2021-08-06 22:37:05 +0300

    refact fl8 test_gendiff

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,38 +1,32 @@
 from gendiff import generate_diff
-from gendiff.format import plain
-from gendiff.format import json
-from gendiff.format import stylish
 import pytest
 import os
 import json
-import yaml
 
 
 path = 'tests/fixtures/'
 
 
-@pytest.mark.parametrize('file1, file2, answer, formatter', 
+@pytest.mark.parametrize('file1, file2, answer, formatter',
                          [('test_01_before.json', 'test_01_after.json',
                            'test_answer_stylish.txt', 'stylish'),
-                         ('test_02_before.yml', 'test_02_after.yml', 
-                          'test_answer_stylish.txt', 'stylish'),
-                         ('test_03_before.json', 'test_03_after.json', 
-                          'test_answer_stylish_2.txt', 'stylish')]
-                        )
+                          ('test_02_before.yml', 'test_02_after.yml',
+                           'test_answer_stylish.txt', 'stylish'),
+                          ('test_03_before.json', 'test_03_after.json',
+                           'test_answer_stylish_2.txt', 'stylish')])
 def test_stylish(file1, file2, answer, formatter):
   with open(os.path.join(path, answer)) as f:
     expected_result = f.read()
   result = generate_diff(os.path.join(path, file1),
-                         os.path.join(path, file2), 'stylish')
+                        os.path.join(path, file2), 'stylish')
   assert result == expected_result
 
 
 @pytest.mark.parametrize('file1, file2, answer', 
-                         [('test_03_before.json', 'test_03_after.json', 
+                         [('test_03_before.json', 'test_03_after.json',
                            'test_answer_plain.txt'),
-                         ('test_04_before.yml', 'test_04_after.yml', 
-                          'test_answer_plain.txt')]
-                        )
+                          ('test_04_before.yml', 'test_04_after.yml',
+                           'test_answer_plain.txt')])
 def test_plain(file1, file2, answer):
   with open(os.path.join(path, answer)) as f:
     expected_result = f.read()
@@ -41,12 +35,11 @@ def test_plain(file1, file2, answer):
   assert result == expected_result
 
 
-@pytest.mark.parametrize('file1, file2, answer', 
-                         [('test_01_before.json', 'test_01_after.json', 
+@pytest.mark.parametrize('file1, file2, answer',
+                         [('test_01_before.json', 'test_01_after.json',
                            'test_answer_json.json'),
-                         ('test_02_before.yml', 'test_02_after.yml', 
-                          'test_answer_json.json')]
-                        )
+                         ('test_02_before.yml', 'test_02_after.yml',
+                          'test_answer_json.json')])
 def test_json(file1, file2, answer):
   with open(os.path.join(path, 'test_answer_json.json')) as f:
     check = json.load(f)
